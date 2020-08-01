d9cb50f59a6161dc38e97c48c45332bf9a31085c
cyasmani <yasmanicastaneda@yahoo.com>
2020-07-31 18:14:29 -0700

    built components and rendered them to webpage

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from "axios";
+import styled from "styled-components";
+import Character from "../src/components/Character.js";
+
+
+// function App() {
+//   const [mainCharacter, setCharacter] = useState([]);
+//   const [sideCharacter, setSideCharacter] = useState();
+//   const MainHeader = styled.h1`
+//   color: red;
+//   `;
+
+//   const SideCharacter = styled.h2`
+//   color:blue;
+  
+//   `
+
+//   useEffect(() => {
+//     axios.get("https://swapi.dev/api/films/1/")
+//       .then(response => {
+//         console.log("Success", response);
+//         setCharacter(response.data.characters);
+        
+//       })
+//       .catch(error => {
+//         console.log("error", error);
+//       })
+
+
+
+//   },[])
+
+  
+
+
+  
+
+
 
 const App = () => {
+  const [mainCharacter, setCharacter] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/films/1/")
+      .then(response => {
+        console.log("Success", response);
+        setCharacter(response.data.characters);
+        
+      })
+      .catch(error => {
+        console.log("error", error);
+      })
+
+
+
+  },[])
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,6 +66,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {mainCharacter.map(url => <Character key={url} characterList={url}/>)}
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,40 @@
 // Write your Character component here
+import React, {useState, useEffect} from 'react'
+import axios from "axios";
+import styled from "styled-components";
+
+function Character(props) {
+    
+    const [myCharacter, setCharacters] = useState([])
+
+    
+
+    const MioHeader = styled.h2`
+    color:red
+    
+    `;
+
+    useEffect(() => {
+        axios.get(props.characterList)
+        .then(response => {
+            console.log("we got it", response)
+            setCharacters(response.data.name)
+        })
+        .catch(error => {
+            console.log("Error", error)
+        })
+    
+    })
+ 
+    return (
+        <div>
+            <MioHeader>
+                {myCharacter}
+                
+            </MioHeader>
+            
+        </div>
+    )
+}
+
+export default Character
